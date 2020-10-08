1b513d421b8a667fc0697a2fe4b6941e65541226
Euclides <eneye280@gmail.com>
2020-10-07 19:20:39 -0500

    Add Teleport Player

--- VR_BussinesChallenge_Qubit/Library/PackageCache/com.unity.xr.interaction.toolkit@0.9.4-preview/Runtime/Locomotion/Teleportation/TeleportationAnchor.cs
+++ VR_BussinesChallenge_Qubit/Library/PackageCache/com.unity.xr.interaction.toolkit@0.9.4-preview/Runtime/Locomotion/Teleportation/TeleportationAnchor.cs
@@ -8,9 +8,9 @@ namespace UnityEngine.XR.Interaction.Toolkit
         [SerializeField]
         [Tooltip("The transform that represents the teleportation destination")]
         Transform m_TeleportAnchorTransform;
-        /// <summary>
-        /// The transform that represents the teleportation destination
-        /// </summary>
+        [SerializeField] internal float widthGizmo;
+        [SerializeField] internal float sizeGizmoTransform;
+
         public Transform teleportAnchorTransform { get { return m_TeleportAnchorTransform; } set { m_TeleportAnchorTransform = value; } }
 
         private void OnValidate()
@@ -32,12 +32,13 @@ namespace UnityEngine.XR.Interaction.Toolkit
             teleportRequest.destinationForwardVector = m_TeleportAnchorTransform.forward;
             return true;
         }
+
         private void OnDrawGizmos()
         {     
             Gizmos.color = Color.blue;
-            GizmoHelpers.DrawWireCubeOriented(m_TeleportAnchorTransform.position, m_TeleportAnchorTransform.rotation, 1.0f);
+            GizmoHelpers.DrawWireCubeOriented(m_TeleportAnchorTransform.position, m_TeleportAnchorTransform.rotation, widthGizmo);
 
-            GizmoHelpers.DrawAxisArrows(m_TeleportAnchorTransform, 1.0f);
+            GizmoHelpers.DrawAxisArrows(m_TeleportAnchorTransform, sizeGizmoTransform);
         }
     }
 }
